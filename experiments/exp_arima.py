e7cd1269b81dd69d9dc943551841da1317e7db71
Pessoa <ppessoa@asu.edu>
2025-04-30 16:21:30 -0700

    gluonts_running

--- experiments/exp_arima.py
+++ experiments/exp_arima.py
@@ -10,10 +10,12 @@ import argparse
 import gc
 
 from gluonts_utils import original_shape,save_forecast_outputs
+
 from gluonts.dataset.repository import get_dataset, dataset_names
 from gluonts.dataset.util import to_pandas
 from gluonts.dataset.common import ListDataset
 from gluonts.evaluation import make_evaluation_predictions
+
 
 # Define parser
 parser = argparse.ArgumentParser()
@@ -23,7 +25,7 @@ parser.add_argument('--folder', type=str, default='ECL_Linear_96_96_S_Mamba_cust
 parser.add_argument('--prediction_length', type=int, default=96)
 parser.add_argument('--past_length', type=int, default=96)
 parser.add_argument('--mamba_batch_size', type=int, default=16)
-parser.add_argument('--Method', type=str, default='DeepAR')
+parser.add_argument('--Method', type=str, default='ARIMA')
 #save_bounds = True  # <-- Set True to also save lower/upper bounds
 parser.add_argument('--save_bounds', type=bool, default=True)
 
@@ -62,62 +64,31 @@ print(test_shape)
 del past
 del fut
 
-past = np.load(folder+'/train_dataset/train_input.npy')
-fut  = np.load(folder+'/train_dataset/train_trues.npy')
-
-past = np.transpose(past, (0, 2, 1)).reshape(-1,past_length)
-fut  = np.transpose(fut , (0, 2, 1)).reshape(-1,prediction_length)
-
-custom_dataset = np.hstack((past,fut))
-print(custom_dataset.shape)
-del past
-del fut
-
-# if 'Traffic' in folder:
-#     print(custom_dataset.shape)
-#     ind = np.linspace(0,len(custom_dataset)-1,7500000).astype(int)
-#     custom_dataset = custom_dataset[ind]
-
-print(test_dataset.shape,custom_dataset.shape)
-
 # %%
+# def original_shape(array, shape=test_shape):
+#     reshaped = array.reshape(-1, shape[2], array.shape[-1])
+#     return reshaped.transpose(0, 2, 1)
 
-def process_single_forecast(test_sample, predictor, prediction_length, quantiles, save_bounds=save_bounds):
-    """
-    Process a single time series forecast and return forecast stats.
-    """
+# def save_forecast_outputs(folder_path, medians, means, futures, within_intervals, confidence, 
+#                            lower_bounds=None, upper_bounds=None, save_bounds=save_bounds,original_shape=original_shape):
+#     os.makedirs(folder_path, exist_ok=True)
 
-    #forecast_it, ts_it = test_sample
-    forecast_it, ts_it = make_evaluation_predictions(
-        dataset=test_sample,
-        predictor=predictor,
-        num_samples=1000,
-    )
-    forecast = list(forecast_it)[0]
-    ts = list(ts_it)[0].to_numpy().reshape(-1)
+#     medians_array = np.array(medians)
+#     np.save(os.path.join(folder_path, 'medians.npy'), original_shape(medians_array))
+#     print('Median avg error:', np.abs(medians_array - np.array(futures)).mean(axis=0))
 
-    past, fut = ts[:-prediction_length], ts[-prediction_length:]
+#     means_array = np.array(means)
+#     np.save(os.path.join(folder_path, 'means.npy'), original_shape(means_array))
+#     print('Mean avg error:', np.abs(means_array - np.array(futures)).mean(axis=0))
 
-    forecast_means = forecast.mean
-    forecast_medians = forecast.median
-    forecast_futures = fut
+#     for k in range(3):
+#         np.save(os.path.join(folder_path, f'sigma{k+1}.npy'), original_shape(np.array(within_intervals[k])))
+#         print(f'Within {confidence[k]*100:.1f}% interval:', np.array(within_intervals[k]).mean())
 
-    forecast_within_intervals = []
-    forecast_lower_bounds = []
-    forecast_upper_bounds = []
+#         if save_bounds and lower_bounds is not None and upper_bounds is not None:
+#             np.save(os.path.join(folder_path, f'sigma{k+1}_lower.npy'), original_shape(np.array(lower_bounds[k])))
+#             np.save(os.path.join(folder_path, f'sigma{k+1}_upper.npy'), original_shape(np.array(upper_bounds[k])))
 
-    for k in range(3):
-        lower = forecast.quantile(quantiles[k, 0])
-        upper = forecast.quantile(quantiles[k, 1])
-        within = np.logical_and(fut > lower, fut < upper)
-        forecast_within_intervals.append(within)
-
-        if save_bounds:
-            forecast_lower_bounds.append(lower)
-            forecast_upper_bounds.append(upper)
-
-    return (forecast_means, forecast_medians, forecast_futures, 
-            forecast_within_intervals, forecast_lower_bounds, forecast_upper_bounds)
 
 perbatch = test_shape[-1]
 batch_size = mamba_batch_size * perbatch
@@ -126,35 +97,34 @@ print(f"Batch size: {batch_size}")
 freq = "1H"
 start = pd.Period("01-01-2019", freq=freq)  # Just part of gluonts test, nothing to be attributed from this
 
-# %%
-# train dataset: 
-train_ds = ListDataset(
-    [{"target": x, "start": start} for x in custom_dataset],
-    freq=freq,
-)
+# # %%
+# # train dataset: 
+# train_ds = ListDataset(
+#     [{"target": x, "start": start} for x in custom_dataset],
+#     freq=freq,
+# )
 
 # test dataset: 
+#This is necessary bc ARIMA as implemeented doesnt do this automatically
+center = test_dataset.mean(axis=1).reshape(-1,1)
+width = test_dataset.std(axis=1).reshape(-1,1)
 test_ds = ListDataset(
-    [{"target": x, "start": start} for x in test_dataset], freq=freq
+    [{"target": x, "start": start} for x in ((test_dataset-center)/width)], freq=freq
 )
 
-if Method == 'DeepAR':
-    from gluonts.torch.model.deepar import DeepAREstimator
-    estimator = DeepAREstimator(
-        freq=freq,
-        batch_size=perbatch,
-        prediction_length=prediction_length,
-        context_length=past_length,
-        trainer_kwargs=dict(
-            max_epochs=20,
-            accelerator="gpu",  # <- use "cpu" if needed
-            devices=1
-        ),
-    )
-    predictor = estimator.train(train_ds)
 
-del custom_dataset
-del train_ds
+if Method == 'ARIMA':
+    from gluonts.ext.r_forecast import RForecastPredictor
+    estimator =  RForecastPredictor(
+        freq=freq,
+        method_name="arima",
+        prediction_length=prediction_length,
+    )
+    predictor= estimator.predict(test_ds)
+else:
+    print("this code expects ARIMA")
+# elif Method == 'FeedForward':
+
 
 # --- Configuration ---
 confidence = np.array([0.683, 0.955, 0.998])
@@ -164,6 +134,8 @@ quantiles = (1 + np.array([[-1], [1]]) * confidence).T / 2  # shape (3,2)
 folder_path = os.path.join(folder, 'gluonts', Method)
 os.makedirs(folder_path, exist_ok=True)
 
+
+
 # --- Main loop ---
 means = np.empty((batch_size//4,prediction_length))#[]
 medians = np.empty_like(means)#[]
@@ -172,37 +144,31 @@ within_intervals = [np.empty_like(means) for _ in range(3)]
 lower_bounds = [np.empty_like(means) for _ in range(3)] if save_bounds else None
 upper_bounds = [np.empty_like(means) for _ in range(3)] if save_bounds else None
 
-# forecast_it, ts_it = make_evaluation_predictions(
-#     dataset=test_ds,
-#     predictor=predictor,
-#     num_samples=100,
-# )
-
-# print('startingthis')
-# # Convert iterators to lists
-# forecasts = list(forecast_it)
-# tss = list(ts_it)
-# print('endingthis')
 
 for idx in tqdm(range(batch_size//4)):
     gc.collect()
+    if Method == 'ARIMA':
+        forecast = next(predictor)
+        
+        tss = test_ds[idx]['target']
+        fut = tss[-prediction_length:] #standardized
+        
+        m,s = center[idx],width[idx]
 
-    (mean_val, median_val, future_val, within_vals, lower_vals, upper_vals) = process_single_forecast(
-        test_ds[idx:idx+1], predictor, prediction_length, quantiles, save_bounds=save_bounds
-    )
-    # (mean_val, median_val, future_val, within_vals, lower_vals, upper_vals) = process_single_forecast(
-    #     [forecasts[idx],tss[idx]], predictor, prediction_length, quantiles, save_bounds=save_bounds
-    # )
+        means[idx] = forecast.mean * s + m
+        medians[idx] = forecast.median * s + m
+        futures[idx] = fut
+        for k in range(3):
+            lower = forecast.quantile(quantiles[k, 0])
+            upper = forecast.quantile(quantiles[k, 1])
+            within = np.logical_and(fut > lower, fut < upper)
 
-    means[idx] = mean_val
-    medians[idx] = median_val
-    futures[idx] = future_val
-    for k in range(3):
-        within_intervals[k][idx] = within_vals[k]
+            within_intervals[k][idx] = within #calculated on standardized
 
-        if save_bounds:
-            lower_bounds[k][idx] = lower_vals[k]
-            upper_bounds[k][idx] = upper_vals[k]
+            if save_bounds:
+                lower_bounds[k][idx] = lower  * s + m
+                upper_bounds[k][idx] = upper  * s + m
+                
 
     if (idx+1)%test_shape[-1]==0:
         save_forecast_outputs(folder_path, medians, means, futures, within_intervals, confidence, lower_bounds, upper_bounds,save_bounds,original_shape=lambda x: original_shape(x[:idx+1],test_shape))
--- gluonts_script.sh
+++ gluonts_script.sh
@@ -1,28 +1,54 @@
 export CUDA_VISIBLE_DEVICES=0
 
 #!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh 
 
 # Define folders
 folders=(
-"ECL_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
 "Sines_se_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
-"Traffic_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
 "VDP_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
-)
-
-
-folders=(
+"ECL_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
 "Traffic_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
-"VDP_Linear_96_96_S_Mamba_custom_M_ft96_sl48_ll96_pl512_dm8_nh3_el1_dl512_df1_fctimeF_ebTrue_dtExp_projection_0"
 )
 # Define methods
 methods=(
 "DeepAR"
+"ARIMA"
 )
 
-#Run over all folder-method combinations
 for method in "${methods[@]}"; do
+    if [[ "$method" == "ARIMA" ]]; then
+        script="experiments/exp_arima.py"
+        conda activate gluontsR
+    else
+        script="experiments/exp_gluonts.py"
+        conda activate gluonts
+    fi
+
     for folder in "${folders[@]}"; do
-        python experiments/exp_gluonts.py --folder "$folder" --Method "$method"
+        prediction_length=96
+        past_length=96
+        mamba_batch_size=16
+
+        # Special cases based on folder name
+        if [[ "$folder" == *"Traffic"* ]]; then
+            mamba_batch_size=8
+        fi
+
+        if [[ "$folder" == *"Brownian"* ]]; then
+            prediction_length=128
+            past_length=128
+        fi
+
+        # Select the script based on method
+        echo "$script"
+
+        # Call python with modified arguments
+        python "$script" \
+            --folder "$folder" \
+            --Method "$method" \
+            --prediction_length "$prediction_length" \
+            --past_length "$past_length" \
+            --mamba_batch_size "$mamba_batch_size"
     done
 done
